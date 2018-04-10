e3f550e6e6b4764516cf6df44b9991814b0c1bad
WHQ_NT_DOMAIN\TS023443 <tim.soliday@cerner.com>
2018-04-10 07:10:07 -0600

    Add project files.

---
+++ AnimationWithBlend/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace AnimationWithBlend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            //  Application.
+            this.Close();
+        }
+    }
+}
